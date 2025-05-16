47480704c7b1e6a2eadf7b4fb627dea8dba0ddef
VincentCYR7 <yuren1027@gmail.com>
2025-05-16 17:03:06 +0900

    UI
    HP bar update

---
+++ Assets/Scripts/UI/PlayerPanel.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPanel : MonoBehaviour
+{
+    public GameObject playerObject;
+    private PlayerMovement playerMovement;
+    private Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        player = playerObject.GetComponent<Player>();
+        GetComponentInChildren<JumpDotsUI>().player = playerMovement;
+        GetComponentInChildren<DashDotsUI>().player = playerMovement;
+        GetComponentInChildren<CellHPBar>().player = player;
+        GetComponentInChildren<CellHPBar>().UpdateHPBar();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
