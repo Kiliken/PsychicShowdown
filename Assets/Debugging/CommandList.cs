da1e21b7eb518b899446c893f737c11c5931fb8f
Kiliken <francescopaolomariani7@gmail.com>
2025-05-23 15:07:11 +0900

    Alpha Portrait Test

--- Assets/Debugging/CommandList.cs
+++ Assets/Debugging/CommandList.cs
@@ -13,11 +13,43 @@ public class CommandList {
         SceneManager.LoadScene(x);
     });
 
+    public static DebugCommand<string> RELOAD { get; } = new DebugCommand<string>("reload", "Reload current scene", "reload", (x) => {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    });
+
+    public static DebugCommand<string> CONTROLLERTYPE { get; } = new DebugCommand<string>("controllerType", "Changes the controller input type", "controllerType", (x) => {
+        ControlManager manager;
+        if (x == "ps4")
+        {
+            
+            manager = GameObject.Find("Player").GetComponent<ControlManager>();
+            manager.playstation = true;
+
+            manager = GameObject.Find("Player2").GetComponent<ControlManager>();
+            manager.playstation = true;
+        }
+        else if (x == "xbx")
+        {
+            manager = GameObject.Find("Player").GetComponent<ControlManager>();
+            manager.playstation = false;
+
+            manager = GameObject.Find("Player2").GetComponent<ControlManager>();
+            manager.playstation = false;
+        }
+    });
+
+    public static DebugCommand<string> EXIT { get; } = new DebugCommand<string>("exit", "Exit the game", "exit", (x) => {
+        Application.Quit();
+    });
+
 
     public static List<object> commandList {get;} = new List<object>
     {
         //Write here your comand too
         LOG,
         LOADSCENE,
+        RELOAD,
+        CONTROLLERTYPE,
+        EXIT,
     };
 }
--- Assets/Debugging/DebugController.cs
+++ Assets/Debugging/DebugController.cs
@@ -95,4 +95,17 @@ public class DebugController : MonoBehaviour
         while (myLogQueue.Count > qsize)
             myLogQueue.Dequeue();
     }
+
+    private static string GetArg(string name)
+    {
+        var args = System.Environment.GetCommandLineArgs();
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == name && args.Length > i + 1)
+            {
+                return args[i + 1];
+            }
+        }
+        return null;
+    }
 }
